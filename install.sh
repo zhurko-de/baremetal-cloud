27a2d8a9020f65030c689b7c2e1d4e5a0727b29c
Oleg Zhurko <ofzhur@gmail.com>
2023-11-05 21:41:39 +0100

    one more typo

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 remote=$1
 
-ssh $remote "git clone --branch revival https://github.com/zhurko-de/baremetal-cloud.git;cd baremetal-cloud/onhost;/bin/bash ./onhost/provision.sh;sleep 10;reboot"
+ssh $remote "git clone --branch revival https://github.com/zhurko-de/baremetal-cloud.git;cd baremetal-cloud/onhost;/bin/bash provision.sh;sleep 10;reboot"
